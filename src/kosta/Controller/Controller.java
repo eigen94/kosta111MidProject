9063da5061564828f71e94a50bf5a62e03a6b037
JYG <JYG@192.168.0.104>
2016-04-15 17:10:03 +0900

    껍데기 올림

--- src/kosta/Controller/Controller.java
+++ src/kosta/Controller/Controller.java
@@ -1,39 +1,65 @@
 package kosta.Controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Controller
- */
+import kosta.action.Action;
+import kosta.action.ActionForward;
+
+
 @WebServlet("/Controller")
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+           
     public Controller() {
         super();
-        // TODO Auto-generated constructor stub
+        
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	String requestURI = request.getRequestURI();
+    	String contextPath = request.getContextPath();
+    	String command = requestURI.substring(contextPath.length()+1);
+    	    	
+    	Action action = null;
+    	ActionForward forward = null;
+    	
+    	if(command.equals(""))
+    	{    		
+    		action = new xxxAction();
+    		forward = action.execute(request, response);
+    	}
+    	
+    	if(forward != null)
+    	{
+    		if(forward.isRedirect())
+    		{
+    			response.sendRedirect(forward.getPath());
+    		}
+    		
+    		else
+    		{
+    			RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
+    			dispatcher.forward(request, response);
+    		}
+    	}
+    	
+    }
+    
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/kosta/action/Action.java
+++ src/kosta/action/Action.java
@@ -1,5 +1,8 @@
 package kosta.action;
 
-public class Action {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+public interface Action {
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response);
 }
