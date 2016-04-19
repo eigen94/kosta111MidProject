427fd129ccdb8735ee2d10c8b0243122c542b7a3
Oh <kosta-23@kosta-23-PC.mshome.net>
2016-04-20 08:54:16 +0900

    체크리스트

---
+++ src/kosta/action/CheckDelete.java
@@ -0,0 +1,27 @@
+package kosta.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kosta.service.Service;
+
+public class CheckDelete implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request,
+			HttpServletResponse response) {
+		int id = Integer.parseInt(request.getParameter("check_id"));
+		int c_id = Integer.parseInt(request.getParameter("check_projectid"));
+		Service service = Service.getInstance();
+		System.out.println(id);
+		System.out.println(c_id);
+		service.CheckDelete(id);
+		
+		ActionForward forward = new ActionForward();
+		forward.setRedirect(true);
+		forward.setPath("projectSelect.do?p_id="+c_id);
+		
+		return forward;
+	}
+
+}
