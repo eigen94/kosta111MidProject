ff32dd6e24b5af041cc7cad05fd33feeaa99c8b9
Dinky <Dinky@192.168.0.11>
2016-04-23 11:36:41 +0900

    커밋할때 업로드 안한 UseCasePrintAction.java 임시 생성 함(김성훈)

---
+++ src/kosta/useCaseAction/UseCasePrintAction.java
@@ -0,0 +1,18 @@
+package kosta.useCaseAction;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kosta.action.Action;
+import kosta.action.ActionForward;
+
+public class UseCasePrintAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request,
+			HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
